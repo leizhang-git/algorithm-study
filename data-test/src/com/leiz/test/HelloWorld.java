c6b7321474bac8ab5693ed18cc0d5b2ab1760202
zhanglei <vip.p@live.com>
2021-08-17 23:35:15 +0800

    Initial commit

---
+++ data-test/src/com/leiz/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.leiz.test;
+
+/**
+ * @Author: zhanglei
+ * @Date: 2021/8/17 23:31
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,Algorithm");
+    }
+}
